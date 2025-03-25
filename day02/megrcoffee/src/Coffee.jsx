7c7817ff9a17a54f930d5df037faf008c3e9e620
somi9954 <jolh999@gmail.com>
2025-03-25 13:31:56 +0900

    day02 수정

---
+++ day02/megrcoffee/src/Coffee.jsx
@@ -0,0 +1,12 @@
+// JSX
+// 1. 함수명 파일명과 동일해야함
+// 2. 함수명은 캐피탈네이밍(첫글자만 대문자)
+// 3. return 하나의 부모태그만 있어야함!
+// 4. return js 코드를 실행하고 싶으면 {} 넣어야 함!
+// 5. style에 {{}} 사용해서 css 문법 넣기 
+const Coffee = () => {
+    return (
+        <div>아메리카노</div>
+    );
+}
+export default Coffee;
