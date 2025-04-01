8ae3c6e5e0dc9488e2b8767b206c379664853841
somi9954 <jolh999@gmail.com>
2025-04-01 19:01:22 +0900

    day06 수정

--- day06/state/src/Register.jsx
+++ day06/state/src/Register.jsx
@@ -14,9 +14,11 @@ const Register = () => {
     <>
       <Input value={value} onChange={setValue} />
       <Text text={"글자길이: 4~12"} valid={validLength(value)} />
-<Text text={"글자에 !,#,@,&,* 중 반드시 포함"} valid={validSpecial(value)} />
-<Text text={"글자에 반드시 숫자 포함!"} valid={validHasNum(value)} />
-
+      <Text
+        text={"글자에 !,#,@,&,* 중 반드시 포함"}
+        valid={validSpecial(value)}
+      />
+      <Text text={"글자에 반드시 숫자 포함!"} valid={validHasNum(value)} />
     </>
   );
 };
